95ba935f4c75ca2ca3662284e6919b735dc649de
poorni1 <poorni@unifytech.com>
2021-03-29 00:39:06 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+//axios.defaults.headers.common['Authorization'] = 'Auth_Token';
+//axios.defaults.headers.post['Content-Type] = 'application/json
+
+axios.interceptors.request.use(
+  (request) => {
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
